b3a103f7d7230ff0580acdf652988311e8691705
Rahul Prasad Shaw <61372250+rjrahul08@users.noreply.github.com>
2023-07-19 13:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  // Use hasOwnProperty method to check if the key exists in the sampleObject
+  return sampleObject.hasOwnProperty(key);
 }
 
 // Do not change the code below
